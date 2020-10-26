86fe151e7b9cb7e1093fe8243dfe4bee41619ea9
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2020-10-26 01:07:53 -0400

    fix multiple use of "parse".  parse is for json parsing while parsedPath is a regex-parsed file path

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* Copyright 2020 Paul Brewer, Economic and Financial Technology Consulting LLC */
 /* This file is open source software.  The MIT License applies to this software. */
 
+/* eslint no-shadow: "error" */
+
 
 const {unzip} = require('unzipit');
 const {parse} = require('secure-json-parse');
@@ -23,9 +25,9 @@ module.exports = async function openzip(zipdataAsPromise, SMRS, progress) {
 
   function simFromJSON(path, s) {
     if (!data.sims) data.sims = [];
-    const parse = simRegex.exec(path);
-    if (!parse) throw new Error("simFromJSON: can not parse path: " + path);
-    const slot = parseInt(parse[1],10);
+    const parsedPath = simRegex.exec(path);
+    if (!parsedPath) throw new Error("simFromJSON: can not parse path: " + path);
+    const slot = parseInt(parsedPath[1],10);
     if (!((slot >= 0) && (slot <= 999)))
       throw new Error("simFromJSON: bad slot " + slot + " in path: " + path);
     const sConfig = parse(s, secureJSONPolicy);
@@ -35,19 +37,19 @@ module.exports = async function openzip(zipdataAsPromise, SMRS, progress) {
 
   function isLogFile(path) {
     let slot, logname;
-    const parse = logRegex.exec(path);
-    if (!parse) return false;
+    const parsedPath = logRegex.exec(path);
+    if (!parsedPath) return false;
     try {
-      slot = parseInt(parse[1],10);
-      logname = parse[2];
+      slot = parseInt(parsedPath[1],10);
+      logname = parsedPath[2];
     } catch (e) {} // eslint-disable-line no-empty
     return ((slot >= 0) && (slot <= 999) && (typeof (logname) === "string") && (SMRS.logNames.indexOf(logname) >= 0));
   }
 
   function restoreLog(path,s) {
-    const parse = logRegex.exec(path);
-    const slot = parseInt(parse[1],10);
-    const logname = parse[2];
+    const parsedPath = logRegex.exec(path);
+    const slot = parseInt(parsedPath[1],10);
+    const logname = parsedPath[2];
     const mylog = data.sims[slot].logs[logname];
     mylog.fromString(s);
   }
